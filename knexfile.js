a078586e5fc7f95afec0e3b338c59756cd85da2a
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-03-26 15:13:41 -0600

    Initial commit

---
+++ knexfile.js
@@ -0,0 +1,36 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/byob',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+
+  // test: {
+  //   client: 'pg',
+  //   connection: 'postgres://localhost/test_byob',
+  //   migrations: {
+  //     directory: './db/migrations'
+  //   },
+  //   seeds: {
+  //     directory: './db/seeds/dev'
+  //   },
+  //   useNullAsDefault: true
+  // },
+
+  // production: {
+  //   client: 'pg',
+  //   connection: process.env.DATABASE_URL + `?ssl=true`,
+  //   migrations: {
+  //     directory: './db/migrations'
+  //   },
+  //   useNullAsDefault: true
+  // }
+
+};
