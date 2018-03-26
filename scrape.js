efd46f0c63b5332c554a63a332ff7512c20c5c1c
parker lindley <fugazi8@gmail.com>
2018-03-26 17:26:22 -0600

    Complete scrape to file

--- scrape.js
+++ scrape.js
@@ -7,9 +7,27 @@ nightmare
   .wait(1000)
   .click('a[href="#tab-2"]')
   .wait(2000)
+  .evaluate( () => {
+    const sites = [...document.querySelector('tbody').childNodes]
+    const superFundSites = sites.map(site => {
+      return {
+        site_name: site.childNodes[0].innerText,
+        city: site.childNodes[2].innerText,
+        county: site.childNodes[4].innerText,
+        reports: site.childNodes[6].childNodes[site.childNodes[6].childNodes.length - 1].getAttribute("href")
+      }
+    })
+    return superFundSites
+  })
   .end()
-  .then( (results ) => {
-    console.log('on page');
+  .then( results => {
+    let output = JSON.stringify(results, null, 2);
+    fs.writeFile('./site-data.json', output, 'utf8', error => {
+      if(error) {
+        return console.log(error)
+      }
+      console.log('File saved');
+    })
   })
   .catch( error => {
     console.log(error);
