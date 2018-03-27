bdf78ee00965b843325e328cf9258bdc508c95dc
parker lindley <fugazi8@gmail.com>
2018-03-27 16:41:29 -0600

    Add initial locations endpoint

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+//LOCATIONS
+app.get('/api/v1/locations', (request, response) => {
+  database('locations').select()
+  .then(locations => response.status(200).json(locations))
+  .catch(error => {
+    return response.status(500).json({ error });
+  })
+})
+
 app.listen(app.get('port'), () => {
   console.log(`Server is running on ${ app.get('port') }`);
 });
--- test/routes.spec.js
+++ test/routes.spec.js
@@ -33,3 +33,42 @@ describe('Client Routes', () => {
     });
   });
 });
+
+describe('API Routes', () => {
+  beforeEach((done) => {
+    database.migrate.rollback()
+    .then(() => {
+      database.migrate.latest()
+      .then(() => {
+        return database.seed.run()
+        .then(() => {
+          done();
+        })
+      })
+    })
+  })
+
+  describe('GET /api/v1/locations', () => {
+    it('should return all of the locations', () => {
+      return chai.request(server)
+      .get('/api/v1/locations')
+      .then( response => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(22);
+        response.body[0].should.have.property('id');
+        response.body[0].id.should.equal(1);
+        response.body[0].should.have.property('city');
+        response.body[0].city.should.equal('Littleton');
+        response.body[0].should.have.property('county');
+        response.body[0].county.should.equal('Jefferson');
+      })
+      .catch(error => {
+        throw error;
+      })
+    })
+  })
+
+
+})
