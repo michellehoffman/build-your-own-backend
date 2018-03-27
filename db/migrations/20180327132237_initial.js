8974037eca78a67e767db61ca298dda7d3ce0dab
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-03-27 13:37:33 -0600

    Create initial migration

---
+++ db/migrations/20180327132237_initial.js
@@ -0,0 +1,26 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('locations', table => {
+      table.increments('id').primary();
+      table.string('city');
+      table.string('county');
+    }),
+
+    knex.schema.createTable('sites', table => {
+      table.increments('id').primary();
+      table.string('name');
+      table.integer('location_id').unsigned()
+      table.foreign('location_id')
+        .references('locations.id');
+      table.string('info');
+    })
+  ]);  
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('locations'),
+    knex.schema.dropTable('sites')
+  ])
+};
