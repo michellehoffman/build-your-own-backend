9a1bebe08323804421c074276843bd4f257726d1
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-03-27 13:17:08 -0600

    Test and set up server file to return html at root url

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.listen(app.get('port'), () => {
+  console.log(`Server is running on ${ app.get('port') }`);
+});
+
+module.exports = app;
